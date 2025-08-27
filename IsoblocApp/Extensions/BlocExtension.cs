f1e8456570a9d4d9391def22f984a832ca672a4d
mimilgrp <mimil.group.49@gmail.com>
2025-08-27 17:51:51 +0200

    Create Bloc and Project extensions for storage files interaction

---
+++ IsoblocApp/Extensions/BlocExtension.cs
@@ -0,0 +1,66 @@
+﻿using IsoblocApp.Models;
+using IsoblocApp.Properties;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace IsoblocApp.Extensions;
+
+public static class BlocExtension
+{
+    private static readonly string csvFile = Resources.blocsCSVFile;
+
+    public static void WriteToFile(this ICollection<Bloc> blocs)
+    {
+        try
+        {
+            using var writer = new StreamWriter(csvFile);
+            writer.WriteLine("Checked;Type;Longueur;Hauteur;Epaisseur;Reservation");
+
+            blocs = [.. blocs.OrderBy(b => b.Type)];
+
+            foreach (var bloc in blocs)
+            {
+                var line = $"{bloc.Checked};{bloc.Type.ToLower()};{bloc.Longueur};{bloc.Hauteur};{bloc.Epaisseur};{bloc.Reservation.ToLower()}";
+                writer.WriteLine(line);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine($"Error writing to file '{csvFile}': {e.Message}");
+        }
+
+    }
+
+    public static List<Bloc> ReadFromFile()
+    {
+        var blocs = new List<Bloc>();
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+        if (File.Exists(csvFile))
+        {
+            using var reader = new StreamReader(csvFile);
+            reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var elements = line?.Split(';');
+
+                if (elements != null && elements.Length == 6)
+                {
+                    blocs.Add(new Bloc
+                    {
+                        Checked = bool.Parse(elements[0]),
+                        Type = elements[1].ToUpper(),
+                        Longueur = int.Parse(elements[2]),
+                        Hauteur = int.Parse(elements[3]),
+                        Epaisseur = int.Parse(elements[4]),
+                        Reservation = textInfo.ToTitleCase(elements[5].ToLower())
+                    });
+                }
+            }
+        }
+
+        return blocs;
+    }
+}
---
+++ IsoblocApp/Extensions/ProjectExtension.cs
@@ -0,0 +1,33 @@
+﻿using IsoblocApp.Models;
+using IsoblocApp.Properties;
+using System.Text.Json;
+
+namespace IsoblocApp.Extensions;
+
+public static class ProjectExtension
+{
+    private static readonly string jsonFile = Resources.projectsJSONFile;
+
+    private static readonly JsonSerializerOptions options = new() { WriteIndented = true };
+
+    public static void WriteToFile(this ICollection<Project> projects)
+    {
+        try
+        {
+            string jsonString = JsonSerializer.Serialize(projects, options);
+
+            File.WriteAllText(jsonFile, jsonString);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erreur impossible d'écrire dans le fichier '{jsonFile}': {e.Message}");
+        }
+    }
+
+    public static List<Project> ReadFromFile()
+    {
+        string jsonString = File.ReadAllText(jsonFile);
+
+        return JsonSerializer.Deserialize<List<Project>>(jsonString) ?? [];
+    }
+}
